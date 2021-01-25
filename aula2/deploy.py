2ce26fa04eaef6ffa764ae8a7af787504f6b096b
pradovalmur <pradovalmur@gmail.com>
2021-01-24 23:03:33 -0300

    arruma bug deploy.py

--- aula2/deploy.py
+++ aula2/deploy.py
@@ -6,10 +6,11 @@ import os
 logging.getLogger().setLevel(logging.INFO)
 cloudformation_client = boto3.client('cloudformation')
 
-def create_stack(stack_name, template_body, **kwaegs):
+
+def create_stack(stack_name, template_body, **kwargs):
     cloudformation_client.create_stack(
         StackName=stack_name,
-        Template_body= template_body,
+        TemplateBody=template_body,
         Capabilities=['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM'],
         TimeoutInMinutes=30,
         OnFailure='ROLLBACK'
@@ -17,11 +18,12 @@ def create_stack(stack_name, template_body, **kwaegs):
 
     cloudformation_client.get_waiter('stack_create_complete').wait(
         StackName=stack_name,
-        WaiterConfig={'Delay':5, 'MaxAttempts':600}
+        WaiterConfig={'Delay': 5, 'MaxAttempts': 600}
     )
 
     cloudformation_client.get_waiter('stack_exists').wait(StackName=stack_name)
     logging.info(f'CREATE COMPLETE')
+
 
 def update_stack(stack_name, template_body, **kwargs):
     try:
@@ -44,6 +46,7 @@ def update_stack(stack_name, template_body, **kwargs):
     cloudformation_client.get_waiter('stack_exists').wait(StackName=stack_name)
     logging.info(f'UPDATE COMPLETE')
 
+
 def get_existing_stacks():
     response = cloudformation_client.list_stacks(
         StackStatusFilter=['CREATE_COMPLETE', 'UPDATE_COMPLETE', 'UPDATE_ROLLBACK_COMPLETE']
@@ -51,8 +54,10 @@ def get_existing_stacks():
 
     return [stack['StackName'] for stack in response['StackSummaries']]
 
+
 def _get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
+
 
 def create_or_update_stack():
     stack_name = 's3-bucket-ci'
@@ -68,5 +73,6 @@ def create_or_update_stack():
         logging.info(f'CREATING STACK {stack_name}')
         create_stack(stack_name, template_body)
 
+
 if __name__ == '__main__':
-    create_or_update_stack()   
+    create_or_update_stack()
